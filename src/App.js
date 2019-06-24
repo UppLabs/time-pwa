64d48cdddb006098d110041390d25c33019a51aa
Roman Hamretskyi <hamretskyi@Romans-MacBook-Pro.local>
2019-06-24 14:53:37 +0300

    Added dynamic time

--- src/App.js
+++ src/App.js
@@ -3,21 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 import dayjs from 'dayjs';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: dayjs(),
+    }
+  }
+  componentDidMount() {
+    this.interval = setInterval(() => this.setState({ time: dayjs() }), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
-  const renderTime = () => {
+renderTime = () => {
     return <h3>
-      {dayjs().format('YYYY-MM-DD hh:mm:ss A')}
+      {this.state.time.format('YYYY-MM-DD hh:mm:ss A')}
     </h3>
   }
-  return (
-    <div className="App">
-      <header className="App-header">
-        {renderTime()}
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.renderTime()}
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
